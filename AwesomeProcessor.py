118611c77d415b20cba807692bee6d21464f6542
IdanOrAqaunt <105841248+IdanOrAqaunt@users.noreply.github.com>
2022-06-30 17:30:14 +0300

    some comments.....

--- AwesomeProcessor.py
+++ AwesomeProcessor.py
@@ -5,3 +5,10 @@ import io
 
 def start_processing(input_csv: io.StringIO):
     df = pd.read_csv(input_csv)
+    #column delta dates
+    #colums count parts replaced
+    #column part importency by price
+    #total cost per visit
+    #normelize all columns
+    
+    #what exactly is the input the train the model
