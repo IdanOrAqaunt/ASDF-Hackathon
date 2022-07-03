438005d44f06889e31496ba57219ac8b45269c9b
idanor <idan.or@aquant.io>
2022-07-03 09:19:11 +0300

    time delta not fixed

--- AwesomeProcessor.py
+++ AwesomeProcessor.py
@@ -4,32 +4,42 @@ import io
 
 class AwesomeProcessor:
 
-    def start_processing(self, input_csv: io.StringIO):
-        df = pd.read_csv(input_csv)
+    def __init__(self, input_csv: io.StringIO):
+        self.__input_path =input_csv
+
+    def start_processing(self):
+        df = pd.read_csv(self.__input_path)
         #column delta dates
-        cool_df = pd.DataFrame()
-        df['visit_date'] = pd.to_datetime(df['visit_date'])
-        df['original_install_date'] = pd.to_datetime(df['original_install_date'])
-        cool_df['installation_to_event_delta'] = df['visit_date'] - df['original_install_date']
+
+        cool_df = df.copy()
+        try:
+            cool_df['visit_date'] = pd.to_datetime(df['visit_date'])
+            cool_df['original_install_date'] = pd.to_datetime(df['original_install_date'])
+            cool_df['installation_to_event_delta'] = df['visit_date'] - df['original_install_date']
 
 
-        #colums count parts replaced
+            #colums count parts replaced
 
-        cool_df['total_relaced_per_asset'] = df.groupby(['asset_id', 'part_id'])['part_id'].transform('nunique')
-        cool_df['customer_name'] = df['customer_name']
-        cool_df['total_relaced_per_asset'] = df.groupby(['asset_id', 'part_id'])['part_id'].transform('nunique')
-        cool_df['total_relaced_per_customer'] = df.groupby(['customer_name'])['part_id'].transform('nunique')
-        cool_df['part_total_replaced_per_customer'] = df.groupby(['customer_name'])['part_id'].transform('nunique')
-        cool_df['total_part_per_name'] = df.groupby(['part_id'])['part_id'].transform('nunique')
-        cool_df['total_part_per_name'] = df['part_id'].value_counts()
-        cool_df['total_part_cost'] = df['total_part_cost']
-        self.normelize(cool_df, 'total_part_per_name', 'total_part_cost', 5, 10)
+            cool_df['total_relaced_per_asset'] = df.groupby(['asset_id', 'part_id'])['part_id'].transform('nunique')
+            cool_df['customer_name'] = df['customer_name']
+            cool_df['total_relaced_per_asset'] = df.groupby(['asset_id', 'part_id'])['part_id'].transform('nunique')
+            cool_df['total_relaced_per_customer'] = df.groupby(['customer_name'])['part_id'].transform('nunique')
+            cool_df['part_total_replaced_per_customer'] = df.groupby(['customer_name'])['part_id'].transform('nunique')
+            cool_df['total_part_per_name'] = df.groupby(['part_id'])['part_id'].transform('nunique')
+            cool_df['total_part_per_name'] = df['part_id'].value_counts()
+            cool_df['total_part_cost'] = df['total_part_cost']
+            np.where(self.normelize(cool_df, 'total_part_per_name', 'total_part_cost', 5, 10) > 50,
+                     np.where())
         #column part importency by price
-
+        except Exception as e:
+            print("exception: ", e)
         #total cost per visit
         #normelize all columns
 
         #what exactly is the input the train the model
+
+
+
     def normelize(self, df: pd.DataFrame, s_from: str, s_to: str, weight_from:int, weight_to:int):
         df1 = df.copy()
         df1['normelized_s_from'] = ((df1[s_from] - df1[s_from].min()) / (
@@ -43,5 +53,4 @@ class AwesomeProcessor:
                     weight_to * df1['normelized_s_to'])
         df1['cust_normalised_overall_score'] = (df1['cast overall score'] - min_potential_score) / (
                     max_potential_score - min_potential_score) * 100
-        df1.loc[df1["template version"] <= 0, 'cust_normalised_overall_score'] = 0
         return df1['cust_normalised_overall_score']
