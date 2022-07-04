403ba631bd8af7b902f1e39187b381d8a06486c5
idanor <idan.or@aquant.io>
2022-07-04 12:10:34 +0300

    for bossssss 2

--- RerieveFiles.py
+++ RerieveFiles.py
@@ -1,4 +1,6 @@
 from S3accessor import S3Accessor
+import csv, re, io
+import zipfile
 
 bucket_name = 'si-input'
 
@@ -11,6 +13,24 @@ class RetrieveFiles:
         for org_name in organizations:
             if "test_" in org_name.url or "Test_" in org_name.url or "_test" in org_name.url or "TEST" in org_name.url or "demo_" in org_name.url:
                 continue
-            items = list(filter(lambda x: x.name == 'metadata.json', self.__S3_accessor.get_files(org_name.url)))
+            items = list(filter(lambda x: x.name == 'output.zip', self.__S3_accessor.get_files(org_name.url)))
             if len(items) == 0:
                 continue
+
+            latestFile = max(items, key=lambda item: item.last_modified)
+
+            output_file = self.__S3_accessor.download_file_content(latestFile.url)
+            try:
+                with zipfile.ZipFile(io.BytesIO(output_file)) as thezip:
+                    for file in thezip.NameToInfo:
+                        if file.split('/')[-1] == 'sc_reference.csv':
+                            # with thezip.open(file, mode='r') as sc_reference:
+                            sc_reference = thezip.read(file)
+                            sc_reference = sc_reference.decode('utf-8').splitlines()
+                            with open(org_name.name + "_sc_reference.csv", "w") as csv_file:
+                                writer = csv.writer(csv_file, delimiter='\t')
+                                for line in sc_reference:
+                                    writer.writerow(re.split('\s+', line))
+                            break
+            except Exception as e:
+                print("exception: ", e)
--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import os
 
 from AwesomeProcessor import AwesomeProcessor
+from RerieveFiles import RetrieveFiles
 
 file_dir = os.path.dirname(os.path.abspath(__file__))
 input_file_path = os.path.join(file_dir, 'sc_reference.csv')
+
+retrieve = RetrieveFiles()
+retrieve.get_files()
 awesome = AwesomeProcessor(input_file_path)
 awesome.start_processing()
