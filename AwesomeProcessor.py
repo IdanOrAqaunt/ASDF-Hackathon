16a168e8c6b8e9941c9b3c1231b4af5007d460fe
idanor <idan.or@aquant.io>
2022-07-04 15:13:41 +0300

    after boss inputs

--- AwesomeProcessor.py
+++ AwesomeProcessor.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 import os
+import random
 
 import numpy as np
 import pandas as pd
@@ -19,16 +20,30 @@ class AwesomeProcessor:
             pd.to_datetime(df['event_date']) - pd.to_datetime(df['original_install_date'])
 
         df['period_age'] = round(df['age'].dt.days / 30)
+        df['period_age'] = df['period_age'].fillna(0)
         df['period_age'] = df['period_age'].astype(int)
         df['cases'] = df.groupby(['period_age'])['period_age'].transform(lambda x: x.count())
-        return df.groupby(['asset_id', 'productype_name', 'period_age']).agg(
+        df['total_cost'] = df['part_cost'] + df['labor_cost']
+        df['warranty_end'] = self.add_warranty(df)
+        df['expected_lifespan'] = self.add_expected_lifespan(df)
+
+        df['cost_new_asset'] = self.add_cost_new_asset(df)
+        df1 = df.groupby(['asset_id', 'producttype_name', 'period_age']).agg(
             cost_sum = pd.NamedAgg(column='total_cost', aggfunc='sum'),
             cases = pd.NamedAgg(column='cases', aggfunc='count'),
             warranty_end = pd.NamedAgg(column='warranty_end', aggfunc='first'),
             expected_lifespan = pd.NamedAgg(column='expected_lifespan', aggfunc='first'),
             cost_new_asset = pd.NamedAgg(column='cost_new_asset', aggfunc='first')
         )
+        return df1
 
+    def add_warranty(self, df:pd.DataFrame):
+        return pd.to_datetime(df['original_install_date']) + pd.DateOffset(months=12)
+    def add_expected_lifespan(self, df:pd.DataFrame):
+        pd.to_datetime(df['original_install_date']) + pd.DateOffset(years=21)
+    def add_cost_new_asset(self, df:pd.DataFrame):
+        max_part_cost = max(df['part_cost'])
+        return np.random.randint(max_part_cost * 3, max_part_cost * 4, size=len(df['asset_id']))
 
     def start_processing(self):
         df = pd.read_csv(self.__input_path)
@@ -61,56 +76,59 @@ class AwesomeProcessor:
             #     {'part_cost': 'sum', 'labor_cost': 'max', 'event_date': 'max'}).sort_values('event_date')
             # df_event_cum['total_cost'] = df_event_cum['part_cost'] + df_event_cum['labor_cost']
 
-            small_data = df[df.model_name == 'GROUP-GROUP CYCLE'][
-                ['investigation_id', 'producttype_name', 'model_name', 'asset_id', 'field_agent_name', 'part_id',
-                 'event_category', 'event_date', 'part_unit_cost', 'part_cost', 'labor_cost', 'original_install_date', 'part_name']]
-            small_data['total_part_per_name'] = small_data.groupby(['part_name', 'producttype_name'])['part_name'].transform('count')
-            q = small_data['total_part_per_name'].quantile(.1)
-            list_of_asset_out = small_data[small_data['total_part_per_name'] > q]['asset_id'].tolist()
-            small_data['event_date'] = pd.to_datetime(small_data['event_date'])
-            small_data['original_install_date'] = pd.to_datetime(small_data['original_install_date'])
-            small_data2 = small_data.groupby(['asset_id', 'investigation_id'], as_index=False).agg(
-                {'part_cost': 'sum', 'labor_cost': 'max', 'event_date': 'max',
-                 'original_install_date': 'max'}).sort_values(['asset_id', 'investigation_id', 'event_date'])
-            # convert to datetime
-            small_data2['diff_time'] = small_data2.groupby('asset_id')['event_date'].diff().dt.days
-            first_visit = (small_data2['event_date'] - small_data2['original_install_date']).dt.days
-            small_data2['diff_time'] = small_data2['diff_time'].fillna(first_visit)
-            # cumulative diff time
-            small_data2['cum_time'] = small_data2.groupby(['asset_id', 'investigation_id'])['diff_time'].sum() \
-                .groupby(level=0).cumsum().reset_index()['diff_time']
-            # total cost
-            small_data2['total_cost'] = small_data2['part_cost'] + small_data2['labor_cost']
-            # cumulative total cost
-            small_data2['cum_cost'] = small_data2.groupby(['asset_id', 'investigation_id'])['total_cost'].sum() \
-                .groupby(level=0).cumsum().reset_index()['total_cost']
+            # small_data = df[df.model_name == 'GROUP-GROUP CYCLE'][
+            #     ['investigation_id', 'producttype_name', 'model_name', 'asset_id', 'field_agent_name', 'part_id',
+            #      'event_category', 'event_date', 'part_unit_cost', 'part_cost', 'labor_cost', 'original_install_date', 'part_name']]
+            # small_data['total_part_per_name'] = small_data.groupby(['part_name', 'producttype_name'])['part_name'].transform('count')
+            # q = small_data['total_part_per_name'].quantile(.1)
+            # list_of_asset_out = small_data[small_data['total_part_per_name'] > q]['asset_id'].tolist()
+            # small_data['event_date'] = pd.to_datetime(small_data['event_date'])
+            # small_data['original_install_date'] = pd.to_datetime(small_data['original_install_date'])
+            # small_data2 = small_data.groupby(['asset_id', 'investigation_id'], as_index=False).agg(
+            #     {'part_cost': 'sum', 'labor_cost': 'max', 'event_date': 'max',
+            #      'original_install_date': 'max'}).sort_values(['asset_id', 'investigation_id', 'event_date'])
+            # # convert to datetime
+            # small_data2['diff_time'] = small_data2.groupby('asset_id')['event_date'].diff().dt.days
+            # first_visit = (small_data2['event_date'] - small_data2['original_install_date']).dt.days
+            # small_data2['diff_time'] = small_data2['diff_time'].fillna(first_visit)
+            # # cumulative diff time
+            # small_data2['cum_time'] = small_data2.groupby(['asset_id', 'investigation_id'])['diff_time'].sum() \
+            #     .groupby(level=0).cumsum().reset_index()['diff_time']
+            # # total cost
+            # small_data2['total_cost'] = small_data2['part_cost'] + small_data2['labor_cost']
+            # # cumulative total cost
+            # small_data2['cum_cost'] = small_data2.groupby(['asset_id', 'investigation_id'])['total_cost'].sum() \
+            #     .groupby(level=0).cumsum().reset_index()['total_cost']
+            #
+            # small_data2['time_class'] = np.where(small_data2['cum_time'] < (365 * 3), '3_year',
+            #                                      np.where(small_data2['cum_time'] < (365 * 6), '6_year',
+            #                                               np.where(small_data2['cum_time'] < (365 * 9), '9_year',
+            #                                                        'Empty')))
+            # small_data2['0.5_years_cost'] = np.where(small_data2['cum_time'] < (365 * 0.5), small_data2['cum_cost'], 0)
+            # small_data2['1_years_cost'] = np.where(small_data2['cum_time'] < (365 * 1.5), small_data2['cum_cost'], 0)
+            # small_data2['1.5_years_cost'] = np.where(small_data2['cum_time'] < (365 * 2), small_data2['cum_cost'], 0)
+            # small_data2['2_years_cost'] = np.where(small_data2['cum_time'] < (365 * 2.5), small_data2['cum_cost'], 0)
+            # small_data2['2.5_years_cost'] = np.where(small_data2['cum_time'] < (365 * 3), small_data2['cum_cost'], 0)
+            # small_data2['3_years_cost'] = np.where(small_data2['cum_time'] < (365 * 3.5), small_data2['cum_cost'], 0)
+            # small_data2['3.5-years_cost'] = np.where(small_data2['cum_time'] < (365 * 4), small_data2['cum_cost'], 0)
+            # small_data2['4_years_cost'] = np.where(small_data2['cum_time'] < (365 * 4.5), small_data2['cum_cost'], 0)
+            # small_data2['4.5_years_cost'] = np.where(small_data2['cum_time'] < (365 * 5), small_data2['cum_cost'], 0)
+            # small_data2['5_years_cost'] = np.where(small_data2['cum_time'] < (365 * 5.5), small_data2['cum_cost'], 0)
+            # small_data2['5.5_years_cost'] = np.where(small_data2['cum_time'] < (365 * 6), small_data2['cum_cost'], 0)
+            # small_data2['6_years_cost'] = np.where(small_data2['cum_time'] < (365 * 6.5), small_data2['cum_cost'], 0)
+            # small_data2['6.5_years_cost'] = np.where(small_data2['cum_time'] < (365 * 7), small_data2['cum_cost'], 0)
+            # small_data2['7_years_cost'] = np.where(small_data2['cum_time'] < (365 * 7.5), small_data2['cum_cost'], 0)
+            # small_data2['7.5_years_cost'] = np.where(small_data2['cum_time'] < (365 * 8), small_data2['cum_cost'], 0)
+            # small_data2 = small_data2.drop('time_class', 1)
+            # # small_data2
+            # small_data2 = small_data2.groupby('asset_id', as_index=False).agg(
+            #     {'original_install_date': 'max', '9_year_cost': 'sum', '6_year_cost': 'sum', '3_year_cost': 'sum'})
+            # # small_data2.filter(small_data2['asset_id'] != list_of_asset_out)
+            # # rows = small_data2.loc[small_data2.asset_id[list_of_asset_out]]
+            # small_data2 = small_data2[~small_data2['asset_id'].isin(list_of_asset_out)]
 
-            small_data2['time_class'] = np.where(small_data2['cum_time'] < (365 * 3), '3_year',
-                                                 np.where(small_data2['cum_time'] < (365 * 6), '6_year',
-                                                          np.where(small_data2['cum_time'] < (365 * 9), '9_year',
-                                                                   'Empty')))
-            small_data2['0.5_years_cost'] = np.where(small_data2['cum_time'] < (365 * 0.5), small_data2['cum_cost'], 0)
-            small_data2['1_years_cost'] = np.where(small_data2['cum_time'] < (365 * 1.5), small_data2['cum_cost'], 0)
-            small_data2['1.5_years_cost'] = np.where(small_data2['cum_time'] < (365 * 2), small_data2['cum_cost'], 0)
-            small_data2['2_years_cost'] = np.where(small_data2['cum_time'] < (365 * 2.5), small_data2['cum_cost'], 0)
-            small_data2['2.5_years_cost'] = np.where(small_data2['cum_time'] < (365 * 3), small_data2['cum_cost'], 0)
-            small_data2['3_years_cost'] = np.where(small_data2['cum_time'] < (365 * 3.5), small_data2['cum_cost'], 0)
-            small_data2['3.5-years_cost'] = np.where(small_data2['cum_time'] < (365 * 4), small_data2['cum_cost'], 0)
-            small_data2['4_years_cost'] = np.where(small_data2['cum_time'] < (365 * 4.5), small_data2['cum_cost'], 0)
-            small_data2['4.5_years_cost'] = np.where(small_data2['cum_time'] < (365 * 5), small_data2['cum_cost'], 0)
-            small_data2['5_years_cost'] = np.where(small_data2['cum_time'] < (365 * 5.5), small_data2['cum_cost'], 0)
-            small_data2['5.5_years_cost'] = np.where(small_data2['cum_time'] < (365 * 6), small_data2['cum_cost'], 0)
-            small_data2['6_years_cost'] = np.where(small_data2['cum_time'] < (365 * 6.5), small_data2['cum_cost'], 0)
-            small_data2['6.5_years_cost'] = np.where(small_data2['cum_time'] < (365 * 7), small_data2['cum_cost'], 0)
-            small_data2['7_years_cost'] = np.where(small_data2['cum_time'] < (365 * 7.5), small_data2['cum_cost'], 0)
-            small_data2['7.5_years_cost'] = np.where(small_data2['cum_time'] < (365 * 8), small_data2['cum_cost'], 0)
-            small_data2 = small_data2.drop('time_class', 1)
-            # small_data2
-            small_data2 = small_data2.groupby('asset_id', as_index=False).agg(
-                {'original_install_date': 'max', '9_year_cost': 'sum', '6_year_cost': 'sum', '3_year_cost': 'sum'})
-            # small_data2.filter(small_data2['asset_id'] != list_of_asset_out)
-            # rows = small_data2.loc[small_data2.asset_id[list_of_asset_out]]
-            small_data2 = small_data2[~small_data2['asset_id'].isin(list_of_asset_out)]
+            small_data2 = self.age_cost(df)
+
             #print the csv
             file_dir = os.path.dirname(os.path.abspath(__file__))
             small_data2.to_csv(os.path.join(file_dir, 'awesome.csv')
--- main.py
+++ main.py
@@ -3,16 +3,16 @@ import os
 from AwesomeProcessor import AwesomeProcessor
 from RerieveFiles import RetrieveFiles
 
-input_file_path = ''
+input_file_path = 'sc_reference.csv'
 file_dir = os.path.dirname(os.path.abspath(__file__))
-retrieve = RetrieveFiles()
-
-for top, dirs, files in os.walk(file_dir):
-    for file_name in files:
-        if not file_name.endswith('sc_reference.csv'):
-            continue
-        input_file_path = os.path.join(file_dir, file_name)
-
-        retrieve.get_files()
-        # awesome = AwesomeProcessor(input_file_path)
-        # awesome.start_processing()
+# retrieve = RetrieveFiles()
+#
+# for top, dirs, files in os.walk(file_dir):
+#     for file_name in files:
+#         if not file_name.endswith('sc_reference.csv'):
+#             continue
+#         input_file_path = os.path.join(file_dir, file_name)
+#
+#         retrieve.get_files()
+awesome = AwesomeProcessor(os.path.join(file_dir, input_file_path))
+awesome.start_processing()
